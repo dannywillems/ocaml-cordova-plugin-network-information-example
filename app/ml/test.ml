00d9739e92cc8d53a931ce3097027ae61cb13cf2
Danny Willems <contact@danny-willems.be>
2016-05-27 21:50:13 +0200

    Add Cordova plugin and change device ready event listener

--- app/ml/test.ml
+++ app/ml/test.ml
@@ -1,12 +1,8 @@
-let device_ready _ =
+let on_device_ready () =
   let d = Cordova_dialogs.t () in
   d#alert
     (Cordova_network_information.connection_to_str
     (Cordova_network_information.current ()))
-    (fun () -> ())
-    ();
-  Js._false
+    ()
 
-let _ =
-  Dom.addEventListener Dom_html.document (Dom.Event.make "deviceready")
-  (Dom_html.handler device_ready) Js._false
+let _ = Cordova.Event.device_ready on_device_ready
